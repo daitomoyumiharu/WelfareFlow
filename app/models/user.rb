1a842ae6fff1f4f92fed46dd319b880287f52fa4
daitomoyumiharu <sakanashi0814@gmail.com>
2023-11-03 08:25:26 +0900

    NameのDB登録が漏れていた為修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
   with_options presence: true do
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
     validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-    validates :nickname
+    validates :name
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
